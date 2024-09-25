9d5dec3eaac9b6a33cfff3ae5993f640596163ce
NemiahHayes <104559735+NemiahHayes@users.noreply.github.com>
2024-09-25 15:20:16 +1200

    First page start

---
+++ gp-portfolio/src/app/cameradev/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Camera() {
+  return (
+    <main>
+      <div className="content">
+        <h1>Nemiah Hayes Portfolio</h1>
+      </div>
+    </main>
+  );
+}
