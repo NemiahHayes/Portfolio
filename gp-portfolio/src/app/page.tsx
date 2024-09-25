9f3e4e671965c116f746c9d0a427c83ebae1cb59
NemiahHayes <hdx5116@autuni.ac.nz>
2024-09-26 09:37:37 +1200

    movin it to my pc

--- gp-portfolio/src/app/page.tsx
+++ gp-portfolio/src/app/page.tsx
@@ -2,17 +2,23 @@ import Link from "next/link";
 
 export default function Home() {
   const navButton =
-    "bg-white text-black p-4 w-32 h-32 flex items-center justify-center rounded-lg hover:bg-white-300 transition";
+    "bg-white text-black p-4 w-32 h-16 flex items-center justify-center rounded-lg hover:bg-gray-600 transition";
 
   return (
     <main>
-      <div className="min-h-screen">
-        <h1>Nemiah Hayes Portfolio</h1>
-
+      <div className="min-h-screen mt-16">
         {/* Buttons */}
-        <div className="flex justify-center space-x-4 mt-8">
-          <Link href="/cameradev" className={navButton}>
-            Camera
+        <div className="flex justify-center space-x-4 mt-24">
+          <Link href="/enginedevelopment" className={navButton}>
+            Engine Dev.
+          </Link>
+
+          <Link href="/generalcplusplus" className={navButton}>
+            General C++
+          </Link>
+
+          <Link href="/misc" className={navButton}>
+            Misc.
           </Link>
         </div>
       </div>
